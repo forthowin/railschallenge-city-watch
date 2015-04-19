6265c22700b3b44360ab8ade88bc343ad22770c5
Tho Nguyen <thogotmail@gmail.com>
2015-04-18 22:47:14 -0500

    show route :id to :name

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get '/responders/:name', to: 'responders#show'
   patch '/responders/:name', to: 'responders#update'
 
+  get '/emergencies/:name', to: 'emergencies#show'
+
   resources :emergencies
   resources :responders, except: [:show, :update]
 end
