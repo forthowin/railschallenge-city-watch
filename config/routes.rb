97b7ef5189b526ddc8e7c89ae9eaeb422f8f97bd
Tho Nguyen <thogotmail@gmail.com>
2015-04-18 21:27:01 -0500

    create routes for responders

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get '/responders/:name', to: 'responders#show'
+  patch '/responders/:name', to: 'responders#update'
+
   resources :emergencies
+  resources :responders, except: [:show, :update]
 end
