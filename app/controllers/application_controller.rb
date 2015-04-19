f08254867fbf15c042faded108548449a4e47002
Tho Nguyen <thogotmail@gmail.com>
2015-04-19 11:16:43 -0500

    refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   def page_not_found
-    render json: Hash['message', 'page not found'].to_json, status: :not_found
+    render json: { message: 'page not found' }, status: :not_found
   end
 end
