1b1110a3f466190530cbf8b114ca5dfe16adfa4b
Tho Nguyen <thogotmail@gmail.com>
2015-04-19 11:08:50 -0500

    fix route ordering

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  resources :emergencies, except: [:show, :update]
+  resources :responders, except: [:show, :update]
+
   get '/responders/:name', to: 'responders#show'
   patch '/responders/:name', to: 'responders#update'
 
   get '/emergencies/:name', to: 'emergencies#show'
   patch '/emergencies/:name', to: 'emergencies#update'
-
-  resources :emergencies, except: [:show, :update]
-  resources :responders, except: [:show, :update]
 end
